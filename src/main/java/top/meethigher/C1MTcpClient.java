514f8eeb579439922c13cb9ebdc4c8b2ecba2e10
meethigher <meethigher@qq.com>
2025-07-19 14:13:42 +0800

    feat: 添加说明

--- src/main/java/top/meethigher/C1MTcpClient.java
+++ src/main/java/top/meethigher/C1MTcpClient.java
@@ -21,6 +21,20 @@ public class C1MTcpClient {
 
 
     public void start() {
+        /**
+         * 如果是在 Centos7 系统上部署该程序，即使配置了{@code net.ipv4.ip_local_port_range = 1024   65535 }
+         * 实际上只能自动分配到 1024-32279这段范围内的端口，无法分配到32280-65535的端口。并且会报错 {@code java.net.BindException: Cannot assign requested address }
+         *
+         * 解决办法有两种
+         * 1. 升级Linux内核。或者使用Windows
+         * 2. 程序上手动bind指定端口
+         *
+         * {@code
+         * Socket socket = new Socket();
+         * socket.bind(new InetSocketAddress(65535));
+         * socket.connect(new InetSocketAddress(host, port));
+         * }
+         */
         for (int i = 0; i < maxConcurrency; i++) {
             final int finalI = i;
             netClient.connect(port, host).onComplete(ar -> {
